62fe2652f1bdd114528b1b2572df0c46918cd982
brandonviaje <viajebrandon@gmail.com>
2026-02-15 12:21:03 -0500

    test: lexer unit tests

--- tests/lexer_test.cpp
+++ tests/lexer_test.cpp
@@ -1,31 +1,74 @@
 #include <gtest/gtest.h>
-#include <fstream>
-#include <sstream>
-#include <string>
 #include "../include/lexer.hpp"
+#include "../include/token.hpp"
 
-class LexerTest : public testing::Test
+class LexerTest : public testing::Test {};
+
+// Test: lexer handles basic return statement
+TEST_F(LexerTest, HandleBasicReturn) 
 {
-protected:
-    std::string content;
+    std::string source = "ret 42;";
+    semp::Lexer lexer(source);
+    auto tokens = lexer.tokenize();
 
-    void SetUp() override
-    {
-        content = load_file("test.semp");
-    }
+    // expect 4 tokens: ret, 42, ;, EOF
+    ASSERT_EQ(tokens.size(), 4);
 
-    std::string load_file(const std::string &file_path)
-    {
-        std::ifstream input(file_path);
-        std::stringstream buffer;
+    EXPECT_EQ(tokens[0].type, semp::TokenType::_RETURN);
+    EXPECT_EQ(tokens[0].lexeme, "ret");
+    EXPECT_EQ(tokens[0].line, 1);
+    EXPECT_EQ(tokens[1].type, semp::TokenType::_INT_LITERAL);
+    EXPECT_EQ(tokens[1].lexeme, "42");
+    EXPECT_EQ(tokens[2].type, semp::TokenType::_SEMICOLON);
+    EXPECT_EQ(tokens[3].type, semp::TokenType::_EOF);
+}
 
-        buffer << input.rdbuf();
-        return buffer.str();
-    }
-};
-
-TEST_F(LexerTest, TestCaseSensitive)
+// Test: lexer ignores whitespace and newlines
+TEST_F(LexerTest, HandleComplexFormatting) 
 {
-    auto content = load_file("test.semp");
-    EXPECT_FALSE(content.empty());
+    std::string source = "  ret    \n  100  ;  ";
+    semp::Lexer lexer(source);
+    auto tokens = lexer.tokenize();
+
+    // expect 4 tokens: ret, 100, ;, EOF
+    ASSERT_EQ(tokens.size(), 4);
+    
+    EXPECT_EQ(tokens[0].type, semp::TokenType::_RETURN);
+    EXPECT_EQ(tokens[1].type, semp::TokenType::_INT_LITERAL);
+    EXPECT_EQ(tokens[1].lexeme, "100");
+    EXPECT_EQ(tokens[1].line, 2); 
+}
+
+// Test: lexer distinguishes keywords from identifiers
+TEST_F(LexerTest, DistinguishKeywordsFromIdentifiers) 
+{
+    std::string source = "ret returnVal ret";
+    semp::Lexer lexer(source);
+    auto tokens = lexer.tokenize();
+
+    // expect 4 tokens: ret, returnVal, ret, EOF
+    ASSERT_EQ(tokens.size(), 4);
+    
+    EXPECT_EQ(tokens[0].type, semp::TokenType::_RETURN);
+    EXPECT_EQ(tokens[1].type, semp::TokenType::_IDENTIFIER);
+    EXPECT_EQ(tokens[1].lexeme, "returnVal"); 
+    EXPECT_EQ(tokens[2].type, semp::TokenType::_RETURN);
+    EXPECT_EQ(tokens[3].type, semp::TokenType::_EOF);
+}
+
+// Test: lexer handles symbols and string literals
+TEST_F(LexerTest, HandleSymbolsAndStrings) 
+{
+    std::string source = "{ \"hello\" }";
+    semp::Lexer lexer(source);
+    auto tokens = lexer.tokenize();
+
+    // expect 4 tokens: {, "hello", }, EOF
+    ASSERT_EQ(tokens.size(), 4);
+
+    EXPECT_EQ(tokens[0].type, semp::TokenType::_LEFT_BRACE);
+    EXPECT_EQ(tokens[1].type, semp::TokenType::_STR_LITERAL);
+    EXPECT_EQ(tokens[1].lexeme, "hello");
+    EXPECT_EQ(tokens[2].type, semp::TokenType::_RIGHT_BRACE);
+    EXPECT_EQ(tokens[3].type, semp::TokenType::_EOF);
 }
