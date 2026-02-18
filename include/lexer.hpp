4aeffa4add2af42d3bec78302ec2d4341cb62d8b
brandonviaje <viajebrandon@gmail.com>
2026-02-18 17:33:05 -0500

    refactor: replace manual index increments with consume/peek helpers

--- include/lexer.hpp
+++ include/lexer.hpp
@@ -5,11 +5,18 @@
 
 namespace semp {
 class Lexer {
- private:
-  std::string contents;
-
  public:
-  Lexer(std::string source_code) : contents{std::move(source_code)} {}
+  Lexer(std::string source_code);
   std::vector<Token> tokenize();
+
+ private:
+  [[nodiscard]] char peek(int offset = 0) const;
+  char consume();
+
+  // member variables
+  std::string m_src;
+  std::size_t m_start;
+  std::size_t m_current;
+  int m_line;
 };
 };  // namespace semp
--- src/lexer.cpp
+++ src/lexer.cpp
@@ -2,100 +2,121 @@
 #include <fstream>
 #include <sstream>
 
+// constructor
+semp::Lexer::Lexer(std::string source_code)
+    : m_src{std::move(source_code)}, m_current{0}, m_start{0}, m_line{1} {}
+
 // convert characters into tokens
 std::vector<semp::Token> semp::Lexer::tokenize() {
   std::vector<semp::Token> tokens;
-  std::size_t i{};
-  int line{1};
 
-  while (i < contents.length()) {
-    char c{contents[i]};
+  // iterate through m_src
+  while (peek() != '\0') {
+    m_start = m_current;  // mark start of new token
+    char c{consume()};
 
-    // skip white spaces
+    // whitespace
     if (std::isspace(c)) {
-      if (c == '\n')
-        line++;
-      i++;
+      if (c == '\n') {
+        m_line++;
+      }
       continue;
     }
 
-    std::size_t start{i};  // mark beginning of the token
-
-    // identifiers/keywords
+    // identifiers/keyword
     if (std::isalpha(c)) {
-      while (i < contents.length() && std::isalnum(contents[i]))
-        i++;
-      std::string_view lexeme =
-          std::string_view(contents).substr(start, i - start);
+      // consume char
+      while (std::isalnum(peek())) {
+        consume();
+      }
 
-      // key words
+      std::string_view lexeme =
+          std::string_view(m_src).substr(m_start, m_current - m_start);
       TokenType type =
           (lexeme == "ret") ? TokenType::_RETURN : TokenType::_IDENTIFIER;
-      tokens.push_back({type, lexeme, line});
+      tokens.push_back({type, lexeme, m_line});
       continue;
     }
 
     // int literal
     if (std::isdigit(c)) {
-      while (i < contents.length() && std::isdigit(contents[i]))
-        i++;
-      tokens.push_back({TokenType::_INT_LITERAL,
-                        std::string_view(contents).substr(start, i - start),
-                        line});
+      // consume int lit
+      while (std::isdigit(peek())) {
+        consume();
+      }
+
+      // parse lexeme
+      std::string_view lexeme =
+          std::string_view(m_src).substr(m_start, m_current - m_start);
+      tokens.push_back(
+          {TokenType::_INT_LITERAL, lexeme, m_line});  // add to tokens
       continue;
     }
 
     // str literal
     if (c == '"') {
-      i++;  // skip opening quote
 
-      while (i < contents.length() && contents[i] != '"') {
-        if (contents[i] == '\n')
-          line++;
-        i++;
+      // scan str lit
+      while (peek() != '"' && peek() != '\0') {
+        if (peek() == '\n') {
+          m_line++;
+        }
+        consume();
       }
+
+      // if we hit EOF before finding second quote, throw err
+      if (peek() == '\0') {
+        throw std::runtime_error("Line " + std::to_string(m_line) +
+                                 ": Unterminated string literal");
+      }
+
+      consume();  // closing quote
 
       // parse lexeme
-      std::string_view lexeme{
-          std::string_view(contents).substr(start + 1, i - start - 1)};
-
-      if (i < contents.length())
-        i++;  // skip closing quote
-      else {
-        std::cerr << "Line " << line << ": Unterminated string literal\n";
-        exit(EXIT_FAILURE);
-      }
-
-      tokens.push_back({TokenType::_STR_LITERAL, lexeme, line});
+      std::string_view lexeme = std::string_view(m_src).substr(
+          m_start + 1, m_current - m_start - 2);  // skip start and end quote
+      tokens.push_back({TokenType::_STR_LITERAL, lexeme, m_line});
       continue;
     }
 
     // punctuation tokens
     switch (c) {
       case ';':
-        tokens.push_back({TokenType::_SEMICOLON, ";", line});
-        i++;
-        continue;
+        tokens.push_back({TokenType::_SEMICOLON, ";", m_line});
+        break;
       case '{':
-        tokens.push_back({TokenType::_LEFT_BRACE, "{", line});
-        i++;
-        continue;
+        tokens.push_back({TokenType::_LEFT_BRACE, "{", m_line});
+        break;
       case '}':
-        tokens.push_back({TokenType::_RIGHT_BRACE, "}", line});
-        i++;
-        continue;
+        tokens.push_back({TokenType::_RIGHT_BRACE, "}", m_line});
+        break;
       default:
-        std::cerr << "Line " << line << ": Unexpected character '" << c
-                  << "'\n";
-        exit(EXIT_FAILURE);
+        throw std::runtime_error("Line " + std::to_string(m_line) +
+                                 ": Unknown character '" + c + "'");
     }
-
-    std::cerr << "Line " << line << ": Unexpected character '" << c << "'\n";
-    exit(EXIT_FAILURE);
   }
 
   // EOF token
-  tokens.push_back({TokenType::_EOF, "", line});
-
+  tokens.push_back({TokenType::_EOF, "", m_line});
   return tokens;
+}
+
+// return character at current pos + offset
+char semp::Lexer::peek(int offset) const {
+  // check if reached EOF
+  if (m_current + offset >= m_src.size()) {
+    return '\0';
+  }
+
+  return m_src[m_current + offset];
+}
+
+// consumes and returns current character, advance index.
+char semp::Lexer::consume() {
+  // check if reached EOF
+  if (m_current >= m_src.size()) {
+    return '\0';
+  }
+
+  return m_src[m_current++];  // consume token and inc index
 }
